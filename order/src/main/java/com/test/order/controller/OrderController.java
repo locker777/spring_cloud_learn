74ee0bc41f302e1dd87c32ef93258d04b7b1227d
zhaojinchuan <locker777@126.com>
2019-05-06 14:35:58 +0800

    视频遗失代码

--- order/src/main/java/com/test/order/controller/OrderController.java
+++ order/src/main/java/com/test/order/controller/OrderController.java
@@ -1,8 +1,25 @@
 package com.test.order.controller;
 
+import com.test.order.VO.ResultVO;
+import com.test.order.converter.OrderForm2OrderDTOConverter;
+import com.test.order.dto.OrderDTO;
+import com.test.order.enums.ResultEnum;
+import com.test.order.exception.OrderException;
+import com.test.order.form.OrderForm;
+import com.test.order.service.OrderService;
+import com.test.order.utils.ResultVOUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Auther: zjc
@@ -11,17 +28,38 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
 
     /**
-     * 1、参数检验
-     * 2、查询商品信息
-     * 3、计算总价
-     * 4、扣库存（调用商品服务）
-     * 5、订单入库
+     * 1. 参数检验
+     * 2. 查询商品信息(调用商品服务)
+     * 3. 计算总价
+     * 4. 扣库存(调用商品服务)
+     * 5. 订单入库
      */
-    public void create(){
+    @PostMapping("/create")
+    public ResultVO<Map<String, String>> create(@Valid OrderForm orderForm,
+                                                BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            log.error("【创建订单】参数不正确, orderForm={}", orderForm);
+            throw new OrderException(ResultEnum.PARAM_ERROR.getCode(),
+                    bindingResult.getFieldError().getDefaultMessage());
+        }
+        // orderForm -> orderDTO
+        OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())) {
+            log.error("【创建订单】购物车信息为空");
+            throw new OrderException(ResultEnum.CART_EMPTY);
+        }
 
+        OrderDTO result = orderService.create(orderDTO);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", result.getOrderId());
+        return ResultVOUtil.success(map);
     }
 }
---
+++ order/src/main/java/com/test/order/converter/OrderForm2OrderDTOConverter.java
@@ -0,0 +1,39 @@
+package com.test.order.converter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.test.order.dataobject.OrderDetail;
+import com.test.order.dto.OrderDTO;
+import com.test.order.enums.ResultEnum;
+import com.test.order.exception.OrderException;
+import com.test.order.form.OrderForm;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class OrderForm2OrderDTOConverter {
+
+    public static OrderDTO convert(OrderForm orderForm) {
+        Gson gson = new Gson();
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setBuyerName(orderForm.getName());
+        orderDTO.setBuyerPhone(orderForm.getPhone());
+        orderDTO.setBuyerAddress(orderForm.getAddress());
+        orderDTO.setBuyerOpenid(orderForm.getOpenid());
+
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        try {
+            orderDetailList = gson.fromJson(orderForm.getItems(),
+                    new TypeToken<List<OrderDetail>>() {
+                    }.getType());
+        } catch (Exception e) {
+            log.error("【json转换】错误, string={}", orderForm.getItems());
+            throw new OrderException(ResultEnum.PARAM_ERROR);
+        }
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
+    }
+}
