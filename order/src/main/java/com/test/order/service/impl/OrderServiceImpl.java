d010f0ba697d7069e6a7c33c5099c6b62541b1b4
zhaojinchuan <locker777@126.com>
2019-05-31 17:27:35 +0800

    补充逻辑

--- order/src/main/java/com/test/order/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/test/order/service/impl/OrderServiceImpl.java
@@ -1,6 +1,9 @@
 package com.test.order.service.impl;
 
+import com.test.order.client.ProductClient;
+import com.test.order.dataobject.OrderDetail;
 import com.test.order.dataobject.OrderMaster;
+import com.test.order.dataobject.ProductInfo;
 import com.test.order.dto.OrderDTO;
 import com.test.order.enums.OrderStatusEnums;
 import com.test.order.enums.PayStatusEnums;
@@ -13,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Auther: zjc
@@ -28,18 +33,36 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderMasterRepository orderMasterRepository;
 
+    @Autowired
+    private ProductClient productClient;
+
     @Override
     public OrderDTO create(OrderDTO orderDTO) {
 
-        // TODO 查询商品信息（调用商品服务）
-        // TODO 计算总价
+        //  查询商品信息（调用商品服务）
+        List<String> productIdList = orderDTO.getOrderDetailList().stream().
+                map(OrderDetail::getProductId).collect(Collectors.toList());
+        List<ProductInfo> productInfoList = productClient.listForOrder(productIdList);
+        //  计算总价
+        BigDecimal orderAmount=new BigDecimal(BigDecimal.ZERO);
+        for (OrderDetail orderDetail : orderDTO.getOrderDetailList()) {
+            for (ProductInfo productInfo: productInfoList) {
+                if(productInfo.getProductId().equals(orderDetail.getProductId())){
+                    orderDetail.getProductPrice()*
+                    //单价*数量
+                }
+                
+            }
+
+        }
+
         // TODO 扣库存（调用商品服务）
 
 
         // 订单入库
-        OrderMaster orderMaster=new OrderMaster();
+        OrderMaster orderMaster = new OrderMaster();
         orderDTO.setOrderId(KeyUtil.genUniqueKey());
-        BeanUtils.copyProperties(orderDTO,orderMaster);
+        BeanUtils.copyProperties(orderDTO, orderMaster);
         orderMaster.setOrderAmount(new BigDecimal(5));
         orderMaster.setOrderStatus(OrderStatusEnums.NEW.getCode());
         orderMaster.setPayStatus(PayStatusEnums.WAIT.getCode());
