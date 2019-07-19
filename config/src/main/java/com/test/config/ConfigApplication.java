7140bd071ea5f8a4c07400e23d75216be86e25fb
zhaojinchuan <locker777@126.com>
2019-07-19 14:27:41 +0800

    配置文件访问规则注解、配置文件存储目录

--- config/src/main/java/com/test/config/ConfigApplication.java
+++ config/src/main/java/com/test/config/ConfigApplication.java
@@ -14,4 +14,15 @@ public class ConfigApplication {
         SpringApplication.run(ConfigApplication.class, args);
     }
 
+    /**
+     * /{name}-{profiles}.yml
+     * /{label}/{name}-{profiles}.yml
+     *
+     *  name 服务名
+     *  profiles 环境
+     *  label 分支（branch）：默认不写 认为是master分支
+     *
+     *  http://127.0.0.1:8082/dev_20190718_config_test_new_branch/order-dev.yml
+     *  http://127.0.0.1:8082/order-test.yml
+     */
 }
