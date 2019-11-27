186b1f0186f64c98e1ce26ca8b308503d5e5e2e8
zhaojinchuan <locker777@126.com>
2019-11-27 14:44:35 +0800

    order  server配置config

---
+++ order/server/src/main/java/com/test/order/controller/EnvController.java
@@ -0,0 +1,24 @@
+package com.test.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: zjc
+ * @Date: 2019/7/19 17:17
+ * @Description:
+ */
+@RestController
+@RequestMapping("/env")
+public class EnvController {
+
+    @Value("${env}")
+    private  String env;
+
+    @GetMapping("/print")
+    public String print(){
+        return env;
+    }
+}
