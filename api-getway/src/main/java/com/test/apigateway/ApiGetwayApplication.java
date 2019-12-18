69ea76a398aa2102a7a6c74249e0d187d42e2877
zhaojinchuan <locker777@126.com>
2019-12-18 17:33:33 +0800

    gateway 接入配置中心 ，bus自动刷新配置未实现

--- api-getway/src/main/java/com/test/apigateway/ApiGetwayApplication.java
+++ api-getway/src/main/java/com/test/apigateway/ApiGetwayApplication.java
@@ -2,7 +2,10 @@ package com.test.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +15,9 @@ public class ApiGetwayApplication {
         SpringApplication.run(ApiGetwayApplication.class, args);
     }
 
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties(){
+        return  new ZuulProperties();
+    }
 }
