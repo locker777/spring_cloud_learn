87d186f9ed22ab10cce683318bcf62dcd35b8625
zhaojinchuan <locker777@126.com>
2020-01-20 16:49:59 +0800

    熔断器设置

--- order/server/src/main/java/com/test/order/controller/HystrixController.java
+++ order/server/src/main/java/com/test/order/controller/HystrixController.java
@@ -19,22 +19,35 @@ import java.util.Arrays;
 public class HystrixController {
 
 
-    @HystrixCommand(commandProperties ={
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")//设置超时时间
-    } )//(fallbackMethod = "fallback")使用上面默认的
+    //超时配置
+//    @HystrixCommand(commandProperties ={
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")//设置超时时间
+//    } )//(fallbackMethod = "fallback")使用上面默认的
+    @HystrixCommand(commandProperties = {
+            //circuitBreaker:断路器
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),//设置熔断
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),//断熔断的最少请求数，默认是10；只有在一个统计窗口内处理的请求数量达到这个阈值，才会进行熔断与否的判断
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),//时间窗口 休眠时间（这个时间内进入降级逻辑） 休眠十秒 指定多久超时，单位毫秒。超时进fallback
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),//断路器打开的百分比条件 判断熔断的阈值，默认值50，表示在一个统计窗口内有50%的请求处理失败，会触发熔断
+    })
     @GetMapping("/getProductInfoList")
-    public String getProductInfoList() {
+    public String getProductInfoList(Integer number) {
+
+        if (number % 2 == 0) {
+            return "success";
+        }
+
         RestTemplate restTemplate = new RestTemplate();
 
         return restTemplate.postForObject("http://127.0.0.1:8086/product/listForOrder",
                 Arrays.asList("157875227953464068"), String.class);
     }
 
-    public String fallback(){
+    public String fallback() {
         return "太拥挤了，请稍后再试~~";
     }
 
-    public String defaultFallback(){
+    public String defaultFallback() {
         return "默认提示：太拥挤了，请稍后再试~~";
     }
 }
