4600b96cd7f606c0de88c6c0453182d96fb56bd4
zhaojinchuan <locker777@126.com>
2020-01-20 15:04:52 +0800

    hystrix触发降级

---
+++ order/server/src/main/java/com/test/order/controller/HystrixController.java
@@ -0,0 +1,31 @@
+package com.test.order.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+/**
+ * @Auther: zjc
+ * @Date: 2020/1/19 14:28
+ * @Description:
+ */
+@RestController
+public class HystrixController {
+
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    @GetMapping("/getProductInfoList")
+    public String getProductInfoList() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.postForObject("http://127.0.0.1:8086/product/listForOrder",
+                Arrays.asList("157875227953464068"), String.class);
+    }
+
+    public String fallback(){
+        return "太拥挤了，请稍后再试~~";
+    }
+}
